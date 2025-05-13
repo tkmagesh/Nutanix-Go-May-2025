83238da9428c37c4809c059d4f2eb73e4afa1c5c
Magesh Kuppan <tkmagesh77@gmail.com>
2025-05-13 09:51:31 +0530

    assignment - solution

--- 04-practice/01-problem.go
+++ 04-practice/01-problem.go
@@ -2,3 +2,18 @@
 Write a go program that prints all the prime numbers between 2 and 100
 */
 
+package main
+
+import "fmt"
+
+func main() {
+LOOP:
+	for no := 2; no <= 100; no++ {
+		for i := 2; i <= (no / 2); i++ {
+			if no%i == 0 {
+				continue LOOP
+			}
+		}
+		fmt.Println("Prime No :", no)
+	}
+}
