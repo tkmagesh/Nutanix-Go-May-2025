51a3b711152da2d18cb791f074273674c7f31503
Magesh Kuppan <tkmagesh77@gmail.com>
2025-05-14 12:41:51 +0530

    method overriding & factory functions

--- 11-OO-Programming/05-method-composition.go
+++ 11-OO-Programming/05-method-composition.go
@@ -23,21 +23,53 @@ func (p *Product) ApplyDiscount(discountPercentage float64) /* do not return */ 
 	p.cost = p.cost * ((100 - discountPercentage) / 100)
 }
 
+// PerishableProduct
+type PerishableProduct struct {
+	Product
+	expiry string
+}
+
+func (pp PerishableProduct) WhoAmI() string {
+	return "I am a perishable product : " + strconv.Itoa(pp.id)
+}
+func (pp PerishableProduct) Format() string {
+	return fmt.Sprintf("%s, expiry = %q", pp.Product.Format(), pp.expiry)
+}
+
+// factory function for PerishableProduct
+func NewPerishableProduct(id int, name string, cost float64, expiry string) *PerishableProduct {
+	return &PerishableProduct{
+		Product: Product{
+			id:   id,
+			name: name,
+			cost: cost,
+		},
+		expiry: expiry,
+	}
+}
+
 // Make the following work for a PerishableProduct
 func main() {
 
-	var p Product = Product{
-		id:   100,
-		name: "pen",
-		cost: 10,
-	}
-	fmt.Println(p.WhoAmI())
+	/*
+		var milk = PerishableProduct{
+			Product: Product{
+				id:   101,
+				name: "Milk",
+				cost: 50,
+			},
+			expiry: "2 Days",
+		}
+	*/
+	var milk = NewPerishableProduct(102, "Milk", 50, "2 Days")
 
-	fmt.Println("Before applying discount :", p.Format())
+	fmt.Println(milk.WhoAmI())
 
-	p.ApplyDiscount(10)
+	fmt.Println("Before applying discount :", milk.Format())
 
-	fmt.Println("After applying discount :", p.Format())
+	milk.ApplyDiscount(10)
+
+	fmt.Println("After applying discount :", milk.Format())
 
 }
 
