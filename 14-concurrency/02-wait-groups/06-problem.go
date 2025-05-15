95dbd656b0003e96e62f064bb2d017728f595e01
Magesh Kuppan <tkmagesh77@gmail.com>
2025-05-15 10:43:39 +0530

    problem

---
+++ 14-concurrency/02-wait-groups/06-problem.go
@@ -0,0 +1,19 @@
+/*
+Execute the logic for finding & printing prime numbers concurrently
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+LOOP:
+	for no := 2; no <= 100; no++ {
+		for i := 2; i <= (no / 2); i++ {
+			if no%i == 0 {
+				continue LOOP
+			}
+		}
+		fmt.Println("Prime No :", no)
+	}
+}
